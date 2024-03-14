542ae1e6e81ec345b7a5bb040440584f1b3c6580
mohamedahmed-cloud <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-03-14 11:10:03 +0200

    fix auth issues

--- app/add-problem/page.tsx
+++ app/add-problem/page.tsx
@@ -4,9 +4,7 @@ export default function AddProblemToFireBase() {
   return (
     <div className="bg-dark-layer-1 flex items-center justify-center flex-col pb-10 pt-5">
       <h1 className="text-gray-300"> Add Problem</h1>
-      <div className="w-10/12 md:w-7/12 ">
-        <AddProblem />
-      </div>
+      <div className="w-10/12 md:w-7/12 ">{/* <AddProblem /> */}</div>
     </div>
   )
 }
